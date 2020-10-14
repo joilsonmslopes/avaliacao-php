95f1e7c68dcfad9c1d87f5c28c9aa438f499696e
Joilson M S Lopes <joilsonmirandasl@hotmail.com>
2020-10-14 12:18:42 -0300

    implementing data injection in the product table

--- index.php
+++ index.php
@@ -3,5 +3,14 @@
     include('./views/content.html');
     include('./database/database.php');
 
+    if(isset($_POST['inserir'])) {
+        $nome = $_POST['produto'];
+        $cor = $_POST['cor'];
+
+        $sql = $pdo->prepare("INSERT INTO `produtos` VALUES (null,?,?)");
+
+        $sql->execute([$nome, $cor]);
+    }
+
     include('./views/footer.html');
 ?>
